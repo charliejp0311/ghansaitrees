1b65b7a05d97e5b64ead5a295d53a535027ec1c3
Charlie Pelton <59211665+charliejp0311@users.noreply.github.com>
2021-03-22 09:58:38 -0500

    Trying to figure out what the website will do

--- .history/ghansaitrees-backend/app/controllers/users_controller_20210322095537.rb
+++ .history/ghansaitrees-backend/app/controllers/users_controller_20210322095537.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
     def index
-        # BASICALLY THIS IS GOING TO BE FOR THE MAILING LIST IF YOU WANT 
-        # TO BE A PART LOGIN SO I DON'T WANT TO RENDER THE LOGIN PAGE I 
-        # PROBABLY NEED TO LOOK AT THE REACT SIDE AND DO A TEST ON THE ERB FILE RATHER THAN HERE
         # if logged_in?
         #     @user = Users.find_by(:user_id)
         #     render JSON: SerializerUsers.new(@user)
